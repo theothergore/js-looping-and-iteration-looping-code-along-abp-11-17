9001e86220644d94cd1012e93925dc4399913bfa
Fly J Gore <jennifer@theothergore.com>
2017-12-19 21:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,36 +16,3 @@ function tailsNeverFails(){
     return `You got ${number} tails in a row!`;
     console.log("This is working...")
 }
-
-
-
-
-
-
-//
-// while (number >= 0.5) {
-//   console.log("You got 2 in a row!");
-// }
-
-// function tailsNeverFails(){
-//     while (Math.random() >= 0.5){
-//       number += 1
-//       console.log("You got ${number++} in a row!")
-//     }
-//     console.log("This is working...")
-// }
-
-
-// // const number = 8
-//
-// // function tailsNeverFails(){
-//   return Math.random() >= 0.5;
-// }
-//
-// while (number >= 0.5) {
-//   console.log(--number);
-// }
-//
-// while (tailsNeverFails()) {
-//   console.log('You got ${number} tails in a row!');
-// }
