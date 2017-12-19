5a8395373c5208519245e4197225c43e395e77b6
Fly J Gore <jennifer@theothergore.com>
2017-12-19 20:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges(names){
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+}
+ return names;
+}
+
+const number = 8
+
+function tailsNeverFails(){
+  return Math.random() >= 0.5;
+}
+
+while (number >= 0.5) {
+  console.log("You got 2 in a row!");
+}
+
+
+
+
+
+
+
+
+// // const number = 8
+//
+// // function tailsNeverFails(){
+//   return Math.random() >= 0.5;
+// }
+//
+// while (number >= 0.5) {
+//   console.log(--number);
+// }
+//
+// while (tailsNeverFails()) {
+//   console.log('You got ${number} tails in a row!');
+// }
